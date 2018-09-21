6ef4dc95b019694443340f2e411ba1667c12909f
Guilherme Alberto de Moraes <g.alberto.moraes@gmail.com>
2018-09-21 16:29:02 -0300

    7. Exercise - Embedding a database

--- demo/src/main/java/com/vogella/example/controller/IssueController.java
+++ demo/src/main/java/com/vogella/example/controller/IssueController.java
@@ -4,26 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.vogella.example.entity.IssueReport;
+import com.vogella.example.repository.IssueRepository;
 
 @Controller
 public class IssueController {
+
+	IssueRepository issueRepository;
+
+	public IssueController(IssueRepository issueRepository) {
+		this.issueRepository = issueRepository;
+	}
+
 	@GetMapping("/issuereport")
-	public String getReport(Model model) {
+	public String getReport(Model model, @RequestParam(name = "submitted", required = false) boolean submitted) {
 		model.addAttribute("issuereport", new IssueReport());
+		model.addAttribute("submitted", submitted);
 		return "issues/issuereport_form";
 	}
 
 	@PostMapping(value = "/issuereport")
-	public String submitReport(IssueReport issueReport, Model model) {
-		model.addAttribute("issuereport", new IssueReport());
-		model.addAttribute("submitted", true);
-		return "issues/issuereport_form";
+	public String submitReport(IssueReport issueReport, RedirectAttributes ra) {
+		this.issueRepository.save(issueReport);
+		ra.addAttribute("submitted", true);
+		return "redirect:/issuereport";
 	}
 
 	@GetMapping("/issues")
 	public String getIssues(Model model) {
+		model.addAttribute("issues", this.issueRepository.findAllButPrivate());
 		return "issues/issuereport_list";
 	}
 }
